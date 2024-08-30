4e8f3aea77263675467cf633cd0671267e331ba2
mati <matias@keeper.app>
2024-08-30 10:24:02 -0300

    added comment

--- src/main/java/com/template/Elevator.java
+++ src/main/java/com/template/Elevator.java
@@ -4,7 +4,8 @@ public class Elevator {
 
 
 
-
+    // Implement this class by observing the tests in this repo. From those you will be able
+    // to pick what the constructors params are, what the methods are and what they should do.
 
 
 }
